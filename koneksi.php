8707064cc68e8a87807dde8a55fd6242ae70fabc
cerealbowl <skarum231@gmail.com>
2025-01-21 14:07:54 +0700

    ok

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    /* membuat variabel config berisi fungsi mysqli_connect 
+        untuk menghubungkan ke database
+        host = localhost
+        user = root
+        pass = ""
+        database = bukutamu
+    */
+
+    $config=mysqli_connect("localhost","root","","bukutamu");
+?>
